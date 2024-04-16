8a73873ec67e2a47329cf348ae0ce8c29e9845ff
enesa3 <166509753+enesa3@users.noreply.github.com>
2024-04-16 22:23:07 +0200

    Update HangmanGame.java
    wip

--- src/main/java/HangmanGame.java
+++ src/main/java/HangmanGame.java
@@ -1,24 +1,46 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
-/*
- * =====================================================================
- * Write your implementation for the assignment at the "TODO" sections.
- * For more details, refer to README.md.
- * =====================================================================
- */
 
 public class HangmanGame {
     public static void main(String[] args) {
 
         // Scanner to read the input
         Scanner scanner = new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
 
         // Read the secret word and turn it into an array of characters
         System.out.println("Enter the secret word:");
-        char[] word = scanner.nextLine().toCharArray();
+        char[] wordArr = scanner.nextLine().toCharArray();
+        String[] strichcode = new String[wordArr.length]; // trackt die -
 
-        // TODO: Initialize an array to track guessed letters, a counter for the
-        // number of wrong guesses, etc.
+
+        for (int j = 0; j < wordArr.length; j++) {
+            strichcode[j] ="_";
+
+        }
+        int tries = 6;
+
+
+        while (tries >= 0) {
+            if (tries == 0){ System.out.println("Game over!"); return  ;}
+
+            System.out.println("current progress:"+ Arrays.toString(strichcode));
+            System.out.println("you have "+tries+" wrong guesses left");
+            System.out.println("Player 2, guess the word:");
+            char[] guessArr = scanner1.nextLine().toCharArray();
+            for (int i = 0; i < wordArr.length; i++) {
+            if (guessArr[0] == wordArr[i]) {
+                strichcode[i] = String.valueOf(guessArr[0]);
+            }
+
+
+
+            }
+
+
+        }
+
 
         // TODO: While the game is not over, do the following:
         // 1. Reveal the letters correctly guessed so far.
